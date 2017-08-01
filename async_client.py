b900f1d402dec712e8acdbb5db05887f97b0e0c1
Ankit Goel <ankitgoel888@gmail.com>
2017-08-01 11:53:02 +0530

    changed ETA to Supratim's code of list comprehension

--- async_client.py
+++ async_client.py
@@ -23,23 +23,24 @@ async def main(websites):
         return (total,htmls)
 
 
-checklist = pickle.load( open( "checklist.pkl", "rb" ) )
+checklist = pickle.load( open("C:/Users/Ankit Goel/Dropbox/Python/testrest/Async2/Async2/checklist.pkl", "rb" ) )
 checklist = list(pd.unique(checklist))[0:20000]
 jsondata = [json.dumps({'con': '10000','origin': i[0], 'destination': i[1],'location': i[0],'arratloc': '2017-07-08 17:30:00'}) for i in checklist]
 websites = [['http://localhost:50000/',jsondata[ix]] for ix,i in enumerate(checklist)]
 loop = asyncio.get_event_loop()
 stats = loop.run_until_complete(main(websites))
-errorcount = len([i for i in stats[1] if 'Error' in i[0]])
-newstats ={datetime.strftime(datetime.now(),'%Y-%m-%d %H:%s'):[stats[0],errorcount,len(checklist)]}
-basestats = {'2017-07-29 0:0': [0, 0,0]}
-try:
-    df = pd.read_csv('results.csv')
-    df.columns = ['Time',0,1,2]
-    df = df.set_index('Time')
+print (stats[0],stats[1])
+# errorcount = len([i for i in stats[1] if 'Error' in i[0]])
+# newstats ={datetime.strftime(datetime.now(),'%Y-%m-%d %H:%s'):[stats[0],errorcount,len(checklist)]}
+# basestats = {'2017-07-29 0:0': [0, 0,0]}
+# try:
+#     df = pd.read_csv('results.csv')
+#     df.columns = ['Time',0,1,2]
+#     df = df.set_index('Time')
 
-except:
-    df = pd.DataFrame.from_dict(basestats, orient='index')
-    df.columns = [0,1,2]
-resdf = df.append(pd.DataFrame.from_dict(newstats, orient='index'))
-resdf.to_csv('results.csv')
-print (resdf)
+# except:
+#     df = pd.DataFrame.from_dict(basestats, orient='index')
+#     df.columns = [0,1,2]
+# resdf = df.append(pd.DataFrame.from_dict(newstats, orient='index'))
+# resdf.to_csv('results.csv')
+# print (resdf)
--- eta_async2_server.py
+++ eta_async2_server.py
@@ -3,79 +3,47 @@ from pymongo import MongoClient
 from motor.motor_asyncio import AsyncIOMotorClient
 import async_timeout
 from datetime import datetime, timedelta
-import collections
 import json
-from operator import itemgetter
 import asyncio
 
 
 client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = client.spotonv4
+handling = 3
 
 
 class Con:
     def __init__(self,docknodict,condata):
         self.docno = docknodict['con']
-        self.arratloc = datetime.strptime(docknodict['arratloc'],"%Y-%m-%d %H:%M:%S")
+        self.currtime = datetime.strptime(docknodict['arratloc'],"%Y-%m-%d %H:%M:%S")
         self.origin = docknodict['origin']
-        self.location=docknodict['location']
+        self.currloc=docknodict['location']
         self.destination=docknodict['destination']
-        conpath=condata['conpath']
-        scheduledepsdict={}
-        for i in condata["legdata"].values():
-            scheduledepsdict.update({(i[0],i[1]):i[2]})
-        self.conpath = conpath
-        self.scheduledepsdict = scheduledepsdict
+        self.a=condata #db.master.find_one({'origin':self.currloc,'destination':self.destination})
 
-    def returndata(self):
-        eta = str(self.geteta(self, journey='no'))
-        return {self.docno:eta}
-
-    def nextdepfunc(self,org,dest,arratloc): #check if self required
-        timelist=[]
-        scheduledepsdict=self.scheduledepsdict
-
-        if scheduledepsdict.get((org,dest))!=None:
-            for i in scheduledepsdict.get((org,dest)):
-                nextdep1 = arratloc.replace(hour=int(i[0].split(":")[0]),minute=int(i[0].split(":")[1]),second=0,microsecond=0)
-                nextarr1 = nextdep1+timedelta(hours=i[1])
-                nextdep2 = arratloc.replace(hour=int(i[0].split(":")[0]),minute=int(i[0].split(":")[1]),second=0,microsecond=0)+timedelta(days=1)
-                nextarr2 = nextdep2+timedelta(hours=i[1])
-                if nextdep1>arratloc:
-                    timelist.append([nextdep1,nextarr1])
-                else:
-                    timelist.append([nextdep2,nextarr2])
-            return min(timelist,key=itemgetter(0))
-        else:
-            return [arratloc+timedelta(hours=3),arratloc+timedelta(hours=3)]  #for market and vb movements  --- con can never fail here
+    def func(self,options,currtime,etatype="schedule"):
+	    if options:
+	        currdate = datetime.combine(currtime.date(), datetime.min.time())
+	        opts = [(currdate+timedelta(hours = int(i[0].split(':')[0])+int(i[0].split(':')[1])*1.0/60)) for i in options]
+	        opts = [i if i>currtime else i+timedelta(hours=24) for i in opts]
+	        if etatype=="schedule":
+	            deptime = min(opts)
+	        else:
+	            deptime = currtime + timedelta(hours=3)
+	        tt = options[opts.index(deptime)][1]+handling
+	        arratdesttime = deptime+timedelta(hours = tt)
+	        availableatdesttime = arratdesttime+timedelta(hours = handling)
+	    else:
+	        deptime = currtime
+	        arratdesttime = currtime+timedelta(hours=3)
+	        availableatdesttime = arratdesttime
+	    return (deptime,arratdesttime,availableatdesttime)
 
 
-    def geteta(self,etatype='schedule',journey="yes"):
-        arratloc = self.arratloc
-        conpath = self.conpath
-        etadict = collections.OrderedDict()
-        for i in range (1,len(conpath)):
-            org = conpath[i-1]
-            dest = conpath[i]
-            if conpath[i-1]==conpath[0] and etatype=="market":
-                nextdep = datetime.today()+timedelta(hours=3)
-                etadict.update({(org,dest):(arratloc,nextdep)})
-                arratloc = self.nextdepfunc(org,dest,arratloc)[1]+timedelta(seconds=(self.nextdepfunc(org,dest,arratloc)[1]-self.nextdepfunc(org,dest,arratloc)[0]).total_seconds()*0.1)+timedelta(hours=5)  ##market penalty & unloading and loading hours
-            else:
-                nextdep = self.nextdepfunc(org,dest,arratloc)[0]
-                etadict.update({(org,dest):(arratloc,nextdep)})
-                arratloc = self.nextdepfunc(org,dest,arratloc)[1]+timedelta(hours=5) #unloading hours & loading hours
-        etadict.update({(next(reversed(etadict))[1],next(reversed(etadict))[1]):(arratloc,arratloc)})
-        if journey=="yes":
-            return etadict.items()
-        else:
-            try:
-                return max(max(etadict.values()))
-            except:
-                return None
-
-
-
+    def geteta(self):
+        [self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.currtime,etatype="schedule")}) if i==1 else self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.a['legdata'].get(str(i-1))[3]['depdetails'][2])}) for i in range(1,len(self.a['legdata'])+1)]
+        eta = list(self.a['legdata'].values())[-1][3]['depdetails'][2]
+        return eta
 
 async def handle(request):
     data = await request.json()
@@ -83,7 +51,7 @@ async def handle(request):
     db = request.app['db']
     condata= await db.master.find_one({'origin':data['origin'],'destination': data['destination']})
     try:
-        res = Con(data,condata).returndata()
+        res = Con(data,condata).geteta()
     except:
         res = 'Error'+str(data)
     return web.Response(text=str(res))
