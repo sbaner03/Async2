6c3b18770611a6db672bcc84dc91a7b56e7e09d6
Ankit Goel <ankitgoel888@gmail.com>
2017-08-31 21:36:31 +0530

    added intransit code

--- async_client.py
+++ async_client.py
@@ -5,16 +5,14 @@ from datetime import datetime
 import pickle
 import pandas as pd
 import json
-
-
-
+import numpy as np
 
 async def fetch(session, url,data):
-    with async_timeout.timeout(0):
+    with async_timeout.timeout(0): 
         async with session.get(url, data = data) as response:
-            return await response.json()
+            return await response.json() #1. while you await response, get from url by passing data
 
-async def main(requests):
+async def main(requests): #2. for computing time
     async with aiohttp.ClientSession() as session:
         start = datetime.now()
         responses = [await fetch(session, req[0],req[1]) for req in requests]
@@ -23,19 +21,24 @@ async def main(requests):
         total = (end-start).total_seconds()
         return total,responses
 
-
-df = pd.read_excel('http://spoton.co.in/downloads/HTR_1HR/HTR_1HR.xls')
-df['TIMESTAMP'] = df.apply(lambda x: datetime.strftime(x['TIMESTAMP'],"%Y-%m-%d %H:%M:%S"),axis=1)
-condict = zip(df['Con Number'],df['Origin Branch'],df['Destn Branch'],df['Hub SC Location'],df['TIMESTAMP'])
-
-jsondata = [json.dumps({'con': str(i[0]),'origin': i[1], 'destination': i[2],'location': i[3],'arratloc': i[4]}) for i in condict]
-requests = [['http://localhost:50000/',i] for i in jsondata]
+df1 = pd.read_excel('http://spoton.co.in/downloads/HTR_1HR/HTR_1HR.xls').loc[:,['Con Number',"TIMESTAMP",'Origin Branch','Destn Branch','Hub SC Location']]
+df1['TIMESTAMP'] = df1['TIMESTAMP'].map(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
+df2 = pd.read_excel('http://spoton.co.in/downloads/TCR_UND_2HRS/TCR_UND_ISDEPART_YES_2HRS_NEXT_TO_NEXT.xls').loc[:,['DOCKNO',"DEPARTURE TIME FRM CURLOC",'ORIGIN BRCODE','DESTCD','CURR BRANCHCODE',"THC ETA","DEPARTED FRM CURRLOC THCNO"]]
+df2['DEPARTURE TIME FRM CURLOC'] = df2['DEPARTURE TIME FRM CURLOC'].map(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
+df2['THC ETA'] = df2.apply(lambda x: 'abc' if pd.isnull(x["THC ETA"]) else datetime.strftime(x["THC ETA"],"%Y-%m-%d %H:%M:%S"),axis=1)
+df2.columns=['Con Number',"TIMESTAMP",'Origin Branch','Destn Branch','Hub SC Location','THC ETA','THC NO']
+df = df1.append(df2)
+df = df.fillna('abc')
+condict = zip(df['Con Number'],df['Origin Branch'],df['Destn Branch'],df['Hub SC Location'],df['TIMESTAMP'],df['THC ETA']) #3. docknodict format from inventory (not actually a dict)
+jsondata = [json.dumps({'con': str(i[0]),'origin': i[1], 'destination': i[2],'location': i[3],'arratloc': i[4],'thceta': str(i[5])}) for i in condict] #4. json conversion for each con; list output
+requests = [['http://localhost:50000/',i] for i in jsondata] #5. request format [url,json_per_con]
 loop = asyncio.get_event_loop()
-timetaken,coneta = loop.run_until_complete(main(requests))
-retdf = pd.DataFrame(coneta)
+timetaken,coneta = loop.run_until_complete(main(requests)) #6. pass to main and get time,resposnse as list of tuples
+retdf = pd.DataFrame(coneta) #7. dataframe created from it
 retdf['Con Number'] = retdf.apply(lambda x: int(x['Con Number']), axis=1)
-df = pd.merge(df,retdf, on = 'Con Number', how = 'left')
+df = pd.merge(df,retdf, on = 'Con Number', how = 'left') #8. merging the responses df to inv df
 print (timetaken)
 print (len(df))
+df.to_csv("abc.csv")
 
 
--- eta_async2_server.py
+++ eta_async2_server.py
@@ -1,3 +1,4 @@
+
 from aiohttp import web
 from pymongo import MongoClient
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -5,6 +6,9 @@ import async_timeout
 from datetime import datetime, timedelta
 import json
 import asyncio
+import numpy as np
+import pandas as pd
+import ast
 
 
 #client = AsyncIOMotorClient("mongodb://ankitgoel888:iep54321@cluster0-shard-00-00-ilsaa.mongodb.net:27017,cluster0-shard-00-01-ilsaa.mongodb.net:27017,cluster0-shard-00-02-ilsaa.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
@@ -14,57 +18,60 @@ db = client.spotonv4
 handling = 3
 
 class Con:
-    def __init__(self,docknodict,condata):
+    def __init__(self,docknodict,condata): #1. docknodict and condata --> self (i)docno,(ii)currtime/arratloc,(iii) currloc,destn,(iv)condata
         self.docno = docknodict['con']
         self.currtime = datetime.strptime(docknodict['arratloc'],"%Y-%m-%d %H:%M:%S")
         self.currloc=docknodict['location']
         self.destination=docknodict['destination']
+        self.thceta=docknodict['thceta']
         self.a=condata
 
-    def func(self,options,currtime,etatype="schedule"):
+    def func(self,options,currtime,etatype="schedule",thceta='abc'):
         if options:
-            currdate = datetime.combine(currtime.date(), datetime.min.time())
-            opts = [(currdate+timedelta(hours = int(i[0].split(':')[0])+int(i[0].split(':')[1])*1.0/60)) for i in options]
-            opts = [i if i>currtime else i+timedelta(hours=24) for i in opts]
-            if etatype=="schedule":
-                deptime = min(opts)
+            currdate = datetime.combine(currtime.date(), datetime.min.time()) #2. thisgives 12 midnight of currdate
+            opts = [(currdate+timedelta(hours = int(i[0].split(':')[0])+int(i[0].split(':')[1])*1.0/60)) for i in options] #3. today's schedules
+            opts = [i if i>currtime else i+timedelta(hours=24) for i in opts] #4. forward options
+            if etatype=="schedule": #5. if market
+                deptime = min(opts) 
             else:
                 deptime = currtime + timedelta(hours=3)
-            tt = options[opts.index(deptime)][1]+handling
-            arratdesttime = deptime+timedelta(hours = tt)
-            availableatdesttime = arratdesttime+timedelta(hours = handling)
+            tt = options[opts.index(deptime)][1]+handling #6. select the shortlisted options tt and add handling
+            arratdesttime = deptime+timedelta(hours = tt) if thceta=='abc' else datetime.strptime(self.thceta,"%Y-%m-%d %H:%M:%S") #7. arrival
+            availableatdesttime = arratdesttime+timedelta(hours = handling) #8. unloaded
         else:
-            deptime = currtime
-            arratdesttime = currtime+timedelta(hours=3)
-            availableatdesttime = arratdesttime
+            deptime = currtime #9. not in schedule file so VB - start now
+            arratdesttime = currtime+timedelta(hours=3) #10. vb completion
+            availableatdesttime = arratdesttime #11. VB unloading is zero
         return (deptime,arratdesttime,availableatdesttime)
 
-    def geteta(self,etatype="schedule"):
-        [self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.currtime,etatype)}) if i==1 else self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.a['legdata'].get(str(i-1))[3]['depdetails'][2])}) for i in range(1,len(self.a['legdata'])+1)]
-        eta = self.a["legdata"].get(str(len(self.a["legdata"])))[3]['depdetails'][2]
-        return {'Con Number': str(self.docno),'ETA': datetime.strftime(eta,"%Y-%m-%d %H:%M:%S")}
-    # pre-if code: for first leg please use currtime
-    # post-if code: for subsequent legs please use the available time already updated in penultimate leg since the list comprehension is based on a range
-    def getjourney(self,etatype="schedule"):
-        [self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.currtime,etatype)}) if i==1 else self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.a['legdata'].get(str(i-1))[3]['depdetails'][2])}) for i in range(1,len(self.a['legdata'])+1)]
+    async def geteta(self,etatype="schedule"):
+        [self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.currtime,etatype,self.thceta)}) if i==1 else self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.a['legdata'].get(str(i-1))[3]['depdetails'][2])}) for i in range(1,len(self.a['legdata'])+1)]
+        #12. (i) i = serial of each leg of legdata (ii) getting the current content and appending depdetails (deptime,arrtime,availabletime) (iii) depdetails arrived at via current content[2]-options of legdata and conditional time (iv) conditional time is current of 1st leg else get the appended frame and take its [2] i.e.availabletime
+        eta = self.a["legdata"].get(str(len(self.a["legdata"])))[3]['depdetails'][2] #13. eta is last items depdetails [2] (available)
+        return {'Con Number': str(self.docno),'ETA': datetime.strftime(eta,"%Y-%m-%d %H:%M:%S")} #14. return conno, strtime eta as a dict
+    
+    async def getjourney(self,etatype="schedule"):#15. journey: get entire legdata
+        [self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.currtime,etatype,self.thceta)}) if i==1 else self.a["legdata"].get(str(i)).append({'depdetails':self.func(self.a["legdata"].get(str(i))[2],self.a['legdata'].get(str(i-1))[3]['depdetails'][2])}) for i in range(1,len(self.a['legdata'])+1)]
         return self.a['legdata']
+        
 
-async def handle(request):
-    data = await request.json()
-
-    db = request.app['db']
-    condata= await db.master.find_one({'origin':data['origin'],'destination': data['destination']})
+async def handle(request): #16. asynced the handle func
     try:
-        res = Con(data,condata).geteta()
+    	data = await request.json() #17. while you await response json
     except:
-        res = {'Con Number': str(data['con']),'ETA': 'Error'}
+    	data2 = await request.text()
+    	data = ast.literal_eval(data2)
+    db = request.app['db'] #17 or you wait db request
+    condata= await db.master.find_one({'origin':data['origin'],'destination': data['destination']}) #or you wait the db find
+    try:
+        res = await Con(data,condata).geteta() #18. start another process
+    except:
+        res = {'Con Number': str(data['con']),'ETA': 'Error'} #18. error handler
     return web.json_response(res)
-
-
+    #return web.json_response(Con(data,condata).geteta())
 
 app = web.Application()
 app['db'] = db
 app.router.add_get('/', handle)
-
 
 web.run_app(app, host='localhost', port=50000)
